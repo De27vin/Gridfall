ce7835801af1796d9de4b667226c83f948794ffc
Devin Mugglin <151910900+De27vin@users.noreply.github.com>
2026-07-26 13:00:06 +0200

    Release version v1.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.example.gridfall"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.3.2"
+        versionCode = 7
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
